79743c0f9bcbdda22fb0d2c46ee4e5eea5ac65e2
oxmagdi <miggzat@gmail.com>
2021-02-10 16:40:50 +0200

    sidebar

--- wp-content/themes/AswomTheme/page.php
+++ wp-content/themes/AswomTheme/page.php
@@ -20,9 +20,9 @@
   	<?php 
   	$theChildID = get_the_ID();
   	$theParentID = wp_get_post_parent_id($theChildID);
-  	echo "<br>The child ID = " . $theChildID;
-  	echo "<br>";
-  	echo "The parent ID = " . $theParentID;
+  	// echo "<br>The child ID = " . $theChildID;
+  	// echo "<br>";
+  	// echo "The parent ID = " . $theParentID;
    	if($theParentID){
     ?>
      <div class="metabox metabox--position-up metabox--with-home-link">
@@ -30,13 +30,30 @@
     </div>
    <?php } ?>
     
-<!--     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+    <?php 
+    	$hasChildren = get_pages(array(
+    		"child_of" => get_the_ID()
+    	));
+    	if($theParentID or $hasChildren){
+    ?>
+	    <div class="page-links">
+	      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentID); ?>"><?php echo get_the_title($theParentID); ?></a></h2>
+	      <ul class="min-list">
+	        <?php 
+
+	        	if($theParentID){
+	        		$findChildrenOf = $theParentID;
+	        	}else{
+	        		$findChildrenOf = get_the_ID();
+	        	}
+	        	wp_list_pages(array(
+	        		"title_li" => NULL,
+	        		"child_of" => $findChildrenOf
+	        	));
+	        ?>
+	      </ul>
+	    </div>
+	<?php } ?>
 
     <div class="generic-content">
       <p><?php the_content(); ?></p>
