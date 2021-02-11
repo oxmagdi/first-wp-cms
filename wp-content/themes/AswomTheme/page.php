fac62bebbe8f63c995d02d38e97b0daabd956f58
oxmagdi <miggzat@gmail.com>
2021-02-11 16:37:57 +0200

    blog

--- wp-content/themes/AswomTheme/page.php
+++ wp-content/themes/AswomTheme/page.php
@@ -25,7 +25,7 @@
   	// echo "The parent ID = " . $theParentID;
    	if($theParentID){
     ?>
-     <div class="metabox metabox--position-up metabox--with-home-link">
+    <div class="metabox metabox--position-up metabox--with-home-link">
       <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentID); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
    <?php } ?>
