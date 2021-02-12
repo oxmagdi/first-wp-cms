f92ad2ca614a7d5456b94696b526f4cf80713104
oxmagdi <miggzat@gmail.com>
2021-02-12 22:26:48 +0200

    display events

---
+++ wp-content/themes/AswomTheme/single-event.php
@@ -0,0 +1,27 @@
+<?php 
+
+	get_header();
+	
+	while(have_posts()){
+		the_post();
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
+      <div class="page-banner__intro">
+        <p>Keep up with our latest news.</p>
+      </div>
+    </div>  
+  </div>
+
+  <div class="container container--narrow page-section">
+	<div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    </div>
+
+	<?php the_content(); ?>
+
+  </div>
+
+<?php } get_footer();?>
